1d003ca16080a020992cdae4d87ea7071ae4d1e4
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-25 20:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var isDate = function (input) {
   //   write your code here
-	if(input==new Date()) return true
-	if(input.match(/^\d{4}-\d{2}-\d{2}$/)){ 
-		return true
-	}
-	return false
-// };  
+	if(!input) return false;
+	if(!/^\d{4}-\d{2}-\d{2}$/.test(input)){ 
+		return false
+	} 
+	let date = new Date(input);
+
+    return !isNaN(date);
+};  
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+const input = prompt("Enter Date."); 
 alert(isDate(input));
