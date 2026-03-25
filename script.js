4afcf3e009fa706132daaab13f386c843c8685e7
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-25 19:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var isDate = function (input) {
   //   write your code here
-};
+	if(input.match(/^\d{4}-\d{2}-\d{2}$/)){ 
+		return true
+	}
+	return false
+};  
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
