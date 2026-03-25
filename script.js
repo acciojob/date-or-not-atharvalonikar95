645fecffe6159b1f2d9be3a921c87782ed7d9b5e
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-25 21:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 var isDate = function (input) {
-  //   write your code here 
   if (input instanceof Date) {
-	return !isNaN(input.getTime());
-  }
-	
-  if(!input) return false;
-  if(typeof input === "string" && /^\d{4}-\d{2}-\d{2}$/.test(input)){ 
-	  let date = new Date(input);
-	  return !isNaN(date.getTime());
-   }  
-	return false
+    return !isNaN(input.getTime());
+  } 
 
-};  
+  if (typeof input === "string") {
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(input)) return false;
+
+    let date = new Date(input);
+    let [y, m, d] = input.split("-").map(Number);
+
+    return (
+      date.getFullYear() === y &&
+      date.getMonth() + 1 === m &&
+      date.getDate() === d
+    );
+  }
+
+  let date = new Date(input);
+  return !isNaN(date.getTime());
+}
 
 // Do not change the code below.
 const input = prompt("Enter Date."); 
